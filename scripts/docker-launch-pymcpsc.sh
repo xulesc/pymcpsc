a02c55eaeeca741f66678dbc71230bb18591b4cf
Sharma Anuj <a.sharma@exodussa.gr>
2018-05-16 10:58:58 +0100

    add comments to docker launch script

--- scripts/docker-launch-pymcpsc.sh
+++ scripts/docker-launch-pymcpsc.sh
@@ -1,5 +1,21 @@
 #!/bin/sh -e
 
-xvfb-run python -u /opt/anaconda2/lib/python2.7/site-packages/pymcpsc-0.1-py2.7.egg/pymcpsc/run_pymcpsc.py "$@"
+#
+# This script is a convenience wrapper to enable a user to launch
+# pymcpsc in a docker image. The primary reason for this script
+# to exist is the need to launch pymcpsc with x-server support
+# due to the Qt dependency. If at a future date this dependency
+# is deprecated this script will in all likelihood become
+# redundant.
+#
+# The path to the run script of pymcpsc is explicitly coded into
+# this script. Any parameters passed to this script and forwarded
+# without alteration to the pymcpsc run script
+#
+
+ANACONDA_PATH = "/opt/anaconda2/"
+PYMCPSC_PATH = "$ANACONDA_PATH/lib/python2.7/site-packages/pymcpsc-0.1-py2.7.egg/pymcpsc"
+
+xvfb-run python -u $PYMCPSC_PATH/run_pymcpsc.py "$@"
 
 
