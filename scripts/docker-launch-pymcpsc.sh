3ebb13d297534bfc041b6a904650c3225c313196
Sharma Anuj <a.sharma@exodussa.gr>
2018-06-26 19:59:21 +0100

    add bash trap to display message for segfault

--- scripts/docker-launch-pymcpsc.sh
+++ scripts/docker-launch-pymcpsc.sh
@@ -16,6 +16,10 @@
 ANACONDA_PATH="/opt/anaconda2/"
 PYMCPSC_PATH="$ANACONDA_PATH/lib/python2.7/site-packages/pymcpsc-0.1-py2.7.egg/pymcpsc"
 
+trap 'case $? in
+        139) echo "NOTE: This segfault is a known issue of the xvfb program and unrelated to pymcpsc execution";;
+      esac' EXIT
 xvfb-run python -u $PYMCPSC_PATH/run_pymcpsc.py "$@"
+
 
 
