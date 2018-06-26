f2301bc0df208716f56713f61dc39cc07a770d44
Sharma Anuj <a.sharma@exodussa.gr>
2018-06-26 20:00:43 +0100

    docker image management script

---
+++ scripts/remove_dockers.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#Stop all running containers
+
+docker stop $(docker ps -aq)
+
+#Remove all containers
+
+docker rm $(docker ps -aq)
+
+#Remove all images
+
+docker rmi $(docker images -q)
